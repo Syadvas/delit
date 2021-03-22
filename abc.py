49474873841b49a9580e5c683893d43073479bbb
Shubham Yadav <shubham.y@polynomial.ai>
2021-03-22 17:09:20 +0530

    firstComm

--- abc.py
+++ abc.py
@@ -7,7 +7,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-DRIVER_PATH ="/home/shmyadav90s/Downloads/chromedriver_linux64" 
+DRIVER_PATH ="/home/shmyadav90s/Downloads/chromedriver" 
 #DRIVER_PATH = r"E:\ChromeDriver\chromedriver.exe"
 driver = webdriver.Chrome(executable_path=DRIVER_PATH)
 
